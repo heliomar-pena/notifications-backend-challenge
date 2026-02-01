977a99989060439e1a73c05d8c575364077f1467
Heliomar Pena <penaheliomar@gmail.com>
2026-02-01 10:35:55 -0300

    fix: configured tests e2e in docker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Public()
-  @Get('/health')
+  @Get('/ping')
   ping(): string {
     return this.appService.ping();
   }
--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,99 +1,25 @@
-import { Test } from '@nestjs/testing';
-import { UsersController } from '../src/users/users.controller'; 
-import { UsersService } from '../src/users.service';
-import { UsersRepository } from '../src/users.repository';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { DataSource } from 'typeorm';
-import { InsertResult } from 'typeorm/browser';
-import { DatabaseModule } from '../database/database.module';
-import { ConfigModule } from '../const/config.module';
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { UsersModule } from 'src/users/users.module';
 
-const usersMock = [
-  {
-    id: 1,
-    name: 'Pepe',
-    password: 'longaniza',
-  },
-  {
-    id: 3,
-    name: 'Pep2e',
-    password: 'longaniza',
-  },
-  {
-    id: 6,
-    name: 'Pep32e',
-    password: 'longaniza',
-  },
-];
-
-describe('UsersController', () => {
-  let usersController: UsersController;
-  let usersRepository: UsersRepository;
-  let dataSource: DataSource;
+describe('AppController (e2e)', () => {
+  let app: INestApplication<App>;
 
   beforeEach(async () => {
-    const module = await Test.createTestingModule({
-      imports: [ConfigModule, DatabaseModule, TypeOrmModule.forFeature([User])],
-      controllers: [UsersController],
-      providers: [UsersService, UsersRepository],
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [UsersModule],
     }).compile();
 
-    dataSource = module.get(DataSource);
-    usersRepository = module.get(UsersRepository);
-    usersController = module.get(UsersController);
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
-  afterEach(async () => {
-    if (dataSource) {
-      await dataSource.getRepository(User).clear();
-      await dataSource.destroy();
-    }
-  });
-
-  describe('When create is called', () => {
-    describe('And parameters are fine', () => {
-      let response: InsertResult;
-      beforeEach(async () => {
-        response = await usersController.create({
-          name: 'Test',
-          password: '123',
-        });
-      });
-
-      it('Then should return InsertResult', () => {
-        expect(response).not.toBe(undefined);
-      });
-
-      describe('And findAll method is called', () => {
-        let findAllResult;
-        beforeEach(async () => {
-          findAllResult = await usersController.findAll();
-        });
-
-        it('Then should return the created user', () => {
-          expect(findAllResult).toStrictEqual(
-            expect.arrayContaining([
-              expect.objectContaining({
-                name: 'Test',
-                password: '123',
-              }),
-            ]),
-          );
-        });
-      });
-    });
-  });
-
-  describe('When findAll is called', () => {
-    describe('And repository found one or more users', () => {
-      beforeEach(() => {
-        jest.spyOn(usersRepository, 'findAll').mockResolvedValue(usersMock);
-      });
-
-      it('Then should return an array of users with username and password', async () => {
-        expect(await usersController.findAll()).toBe(usersMock);
-      });
-    });
+  it('/ping (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/ping')
+      .expect(200)
+      .expect('Pong!');
   });
 });
