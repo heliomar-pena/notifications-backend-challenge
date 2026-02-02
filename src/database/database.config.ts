3dbb60c1eb0d181de339bf7dd58d527d6f7064b9
Heliomar Pena <penaheliomar@gmail.com>
2026-02-02 20:02:05 -0300

    fix: removed debugging console log

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,22 +1,18 @@
 import { registerAs } from '@nestjs/config';
 
-const databaseConfig = registerAs('database', () => {
-  console.log({ USERNAME: process.env.DATABASE_USERNAME });
-
-  return {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT
-      ? Number(process.env.DATABASE_PORT)
-      : undefined,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
-    syncronize: process.env.DATABASE_SYNCRONIZE === 'true',
-    migrations:
-      process.env.DATABASE_RUN_SEEDS === 'true'
-        ? [__dirname + '/migrations/**/*.ts', __dirname + '/seeds/**/*.ts']
-        : [__dirname + '/migrations/**/*.ts'],
-  };
-});
+const databaseConfig = registerAs('database', () => ({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT
+    ? Number(process.env.DATABASE_PORT)
+    : undefined,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  syncronize: process.env.DATABASE_SYNCRONIZE === 'true',
+  migrations:
+    process.env.DATABASE_RUN_SEEDS === 'true'
+      ? [__dirname + '/migrations/**/*.ts', __dirname + '/seeds/**/*.ts']
+      : [__dirname + '/migrations/**/*.ts'],
+}));
 
 export default databaseConfig;
